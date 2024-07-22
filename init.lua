3f4a2bff7c0ffac41483262b7c66ff8fa9501d04
Matheus-Ei <t4igacomercial@gmail.com>
2024-07-21 23:03:01 -0300

    undo the autosave temporarilly

--- init.lua
+++ init.lua
@@ -11,25 +11,5 @@ vim.cmd("set timeoutlen=5000")
 -- Set colors to the notifications
 vim.opt.termguicolors = true
 
-
--- Sets the save function
-local function auto_save()
-  if vim.bo.modified then
-    vim.cmd('w')
-  end
-end
-
--- Sets the autosave periodically function
-local function auto_save()
-  local interval = 10000
-  vim.defer_fn(function()
-    auto_save()
-    setup_auto_save()
-  end, interval)
-end
-
-auto_save()
-
-
 -- Load lazy vim package manager
 require("config.lazy")
