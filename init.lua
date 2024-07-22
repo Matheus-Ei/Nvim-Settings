ec8616cbcecb2d9af9ab5bdbc2d0a32cde157b90
Matheus-Ei <t4igacomercial@gmail.com>
2024-07-21 22:51:41 -0300

    add autosave feature

--- init.lua
+++ init.lua
@@ -8,11 +8,28 @@ vim.cmd("set shiftwidth=4")
 vim.g.mapleader = "<Space>"
 vim.cmd("set timeoutlen=5000")
 
--- Set alt+s = Esc
--- vim.keymap.set({'n', 'i', 'v', 'x', 's', 'c', 't', 'o'}, '<C-s>', '<Esc>', { noremap = true, silent = true })
-
 -- Set colors to the notifications
 vim.opt.termguicolors = true
+
+
+-- Sets the save function
+local function auto_save()
+  if vim.bo.modified then
+    vim.cmd('w')
+  end
+end
+
+-- Sets the autosave periodically function
+local function auto_save()
+  local interval = 10000
+  vim.defer_fn(function()
+    auto_save()
+    setup_auto_save()
+  end, interval)
+end
+
+auto_save()
+
 
 -- Load lazy vim package manager
 require("config.lazy")
