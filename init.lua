946e3b6116e9c792c311253198e33d252e822619
Matheus-Ei <t4igacomercial@gmail.com>
2024-07-23 17:03:24 -0300

    add code highlight and change the navigation keys

--- init.lua
+++ init.lua
@@ -20,3 +20,11 @@ vim.cmd("set timeoutlen=5000")
 
 vim.keymap.set('n', '<Leader>s', ':wall<CR>', { desc = "Save all" }) -- Save all files open
 vim.keymap.set({ 'n' }, '<Leader>l', ':Lazy<CR>', { desc = "Open lazy" })
+
+
+local modes = { 'n', 'v', 's', 'o' }
+vim.keymap.set(modes, 'j', 'h', { noremap = true, silent = true })
+vim.keymap.set(modes, 'k', 'j', { noremap = true, silent = true })
+vim.keymap.set(modes, 'l', 'k', { noremap = true, silent = true })
+vim.keymap.set(modes, 'ç', 'l', { noremap = true, silent = true })
+
