ffaec2c415f088f134ee9b4f75454e636973a205
Matheus-Ei <t4igacomercial@gmail.com>
2024-09-12 16:25:18 -0300

    Add references to lang-support shortcuts

--- lua/shortcuts/lang-support.lua
+++ lua/shortcuts/lang-support.lua
@@ -3,8 +3,9 @@ local M = {}
 
 function M.config()
     vim.keymap.set({'i', 'n'}, '<M-h>', vim.lsp.buf.hover, {}) -- Show documentation
-    vim.keymap.set('n', '<M-r>', vim.lsp.buf.rename, {}) -- Rename variables
-    vim.keymap.set('n', '<M-g>', vim.lsp.buf.definition, {}) -- Go to the definition
+    vim.keymap.set('n', '<M-r>', vim.lsp.buf.rename, {})
+    vim.keymap.set('n', '<M-g>', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', '<M-f>', vim.lsp.buf.references, {})
 end
 
 
