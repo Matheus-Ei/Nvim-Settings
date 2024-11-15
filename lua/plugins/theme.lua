9f4e38d98acf03a6dd54035f45e7084d251dcaf0
Matheus-Ei <t4igacomercial@gmail.com>
2024-11-15 14:37:36 -0300

    change themes and remove comments

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,5 @@
 return {
-    -- Github theme
+    --[[     -- Github theme
     -- https://dotfyle.com/plugins/projekt0n/github-nvim-theme
     {
         'projekt0n/github-nvim-theme',
@@ -9,6 +9,65 @@ return {
             require('github-theme').setup({})
 
             vim.cmd('colorscheme github_dark_default')
+        end,
+    },
+ ]]
+
+    -- Catppuccin theme
+    -- https://github.com/catppuccin/nvim
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                background = { -- :h background
+                    light = "latte",
+                    dark = "mocha",
+                },
+                transparent_background = false,
+                show_end_of_buffer = false,
+                term_colors = false,
+                dim_inactive = {
+                    enabled = false,
+                    shade = "dark",
+                    percentage = 0.15,
+                },
+                no_italic = false,
+                no_bold = false,
+                no_underline = false,
+                styles = {
+                    comments = { "italic" },
+                    conditionals = { "italic" },
+                    loops = {},
+                    functions = {},
+                    keywords = {},
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = {},
+                    properties = {},
+                    types = {},
+                    operators = {},
+                },
+                color_overrides = {},
+                custom_highlights = {},
+                default_integrations = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = false,
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "",
+                    },
+                },
+            })
+
+            -- Set the colorscheme
+            vim.cmd.colorscheme("catppuccin")
         end,
     },
 
